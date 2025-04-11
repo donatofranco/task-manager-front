507c7448a78f57ac7f8f4df14efb0f4597686255
donatofranco <francoedonato@gmail.com>
2025-04-11 20:46:01 -0300

    Remove unused var

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ import Modal from '@/components/Modal';
 export default function DashboardPage() {
   const { isAuthenticated } = useAuth();
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [_, setLoading] = useState(true);
+  const [, setLoading] = useState(true);
   const [error, setError] = useState('');
   const router = useRouter();
 
